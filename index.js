722bc2643acfd147377ad3ae8458efb5cd850314
Miguel Espinoza <68973067+10iguel@users.noreply.github.com>
2024-01-10 10:02:40 -0700

    Week 01

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+//import express from "express"
+
+const express = require("express")
+const app = express();
+
+app.get("/", (req, res, next)=>{
+    res.send("Hello World")
+})
+
+app.listen(4321,()=>{
+    console.log("This is working")
+})
+
+
 console.log("hello")
