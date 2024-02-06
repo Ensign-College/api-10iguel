6008a431323eb7f5148f8aa4009b471300b5d35a
Miguel Espinoza <68973067+10iguel@users.noreply.github.com>
2024-02-06 12:53:03 -0700

    completing CORS class

--- index.js
+++ index.js
@@ -6,58 +6,52 @@ const bodyParser = require("body-parser")
 const redisClient = Redis.createClient(
     {url : `redis://localhost:6379`}
 );
-
-
 redisClient.on("connect", ()=>{
     console.log("Connection to Redis")
 })
+const cors = require("cors")
+const options ={
+    origin:'http://localhost:5173',
+}
 
-// redisClient.on("error", ()=>{
-//     console.log("Connection failed")
-// })
 const app = express();
 
-const port = 3000;
+const port = 3001;
 
-// const boxes = [
-//     // {boxId:1},
-//     // {boxId:2},
-//     // {boxId:3},
-//     // {boxId:4},
-// ]
+
+app.use(express.json())
+
+app.use(cors(options))
 
 app.use(bodyParser.json());
-
 
 app.get("/", (req, res, next)=>{
     res.send("Hello World")
 })
+
 app.get("/boxes", async (req, res, next)=>{
     let boxes = await redisClient.json.get('boxes',{
         path: '$' //$ to get the whole object    $.name
     })
-    res.send(JSON.stringify(boxes))
+    res.json(boxes[0])
+    console.log(boxes)
 })
 
 app.post("/boxes", async (req, res )=>{
-
         const newBox = req.body;
         newBox.id =  await redisClient.json.arrLen('boxes','$') +1;
-        //newBox.id =  parseInt( await redisClient.json.arrLen('boxes','$')) +1;
-        // await redisClient.json.set('boxes','$',newBox)
-        await redisClient.json.arrAppend('boxes','$',[newBox])
-
-        // let existingBoxes = await redisClient.json.get("boxes", { path: "$" }) || [];
-        // console.log(existingBoxes)
+        await redisClient.json.arrAppend('boxes','$',{newBox})
+    console.log([newBox])
         res.json({ message: "Box added successfully", newBox });
+    let boxes = await redisClient.json.get('boxes',{
+        path: '$' //$ to get the whole object    $.name
+    })
+    console.log("These are the updated boxes"+ [JSON.stringify(boxes)])
 })
-
-
 
 app.listen(port,()=>{
-    redisClient.connect();
+    redisClient.connect().then(r => {
+        console.log("It works")
+    });
     console.log(`Listening on port ${port}`)
 })
-
-
-console.log("hello")
