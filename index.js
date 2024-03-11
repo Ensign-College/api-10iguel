37728d2a17ac55e9cf08c1d49b88a02834759083
Miguel Espinoza <68973067+10iguel@users.noreply.github.com>
2024-03-11 17:33:16 -0600

    Adding sam template.yml

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
-app.get("/", async (req, res, next) => {
+app.get("/recipes", async (req, res, next) => {
     res.send("Hello World")
 })
 
